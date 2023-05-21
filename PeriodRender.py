33bf8d9974baf5f8e215c064b71bbddb0fc832a9
MachExec <max591b@gmail.com>
2023-05-21 00:03:37 -0700

    Added new User friendly features

--- PeriodRender.py
+++ PeriodRender.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class PeriodRender:
 
-    def __init__(self,I,D):
+    def __init__(self,I,D,S):
 
         self.Image = cv2.imread('Input\\' + I,0)
         self.height, self.width = self.Image.shape 
@@ -15,9 +15,35 @@ class PeriodRender:
 
         self.PixelImage = [[] for _ in range(self.HeightRatio)]
 
+        self.Symbols = S
 
+    def CommonSymbols(self):
+
+        if(self.Symbols=="emoji" or self.Symbols=="Emoji"):
+            self.Symbols = ["⬛","⬜"]
+            return self.Symbols
+
+        elif(self.Symbols=="sqre" or self.Symbols=="Sqre"):
+            self.Symbols = ["⠶"," "]
+            return self.Symbols
+        
+        elif(self.Symbols=="rect" or self.Symbols=="Rect"):
+            self.Symbols = ["⣿"," "]
+            return self.Symbols
+        
+        elif(self.Symbols=="line" or self.Symbols=="Line"):
+            self.Symbols = ["⠉"," "]
+            return self.Symbols
+        
+
+        
+        else:
+            return self.Symbols
 
     def ConvertBinary(self):
+
+        self.Symbols = self.CommonSymbols()
+
         for rows in range(self.height):
             for pixel in range(self.width):
                 
@@ -43,13 +69,13 @@ class PeriodRender:
                 Current = self.Image[rows*self.DownRes][pixel*self.DownRes] 
 
                 if(Current==255): #white
-                    self.PixelImage[rows].append(" ")
+                    self.PixelImage[rows].append(self.Symbols[1])
 
                 elif(Current==0): #black
-                    self.PixelImage[rows].append(".")
+                    self.PixelImage[rows].append(self.Symbols[0])
 
             
-        with open('store.txt', 'w') as file:
+        with open('store.txt', 'w',encoding='utf-8') as file:
             for rows in range(self.HeightRatio):
                 for Pixel in range(self.WidthRatio):
                     file.write(self.PixelImage[rows][Pixel])
@@ -60,4 +86,4 @@ class PeriodRender:
 
 #cv2.imwrite('Output/output_image.jpg', PixelImage)
 
-PeriodRender(I="Walter_White_S5B.png",D=1).FinalRender()
+PeriodRender(I="Walter_White_S5B.png",D=3,S="Line").FinalRender() #first parm of S is black, second is white
