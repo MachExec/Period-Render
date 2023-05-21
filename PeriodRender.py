7e55a2a224322b90eb7c255e1c20f1a50d479592
MachExec <max591b@gmail.com>
2023-05-20 23:45:17 -0700

    Cleaned code and organized into class

--- PeriodRender.py
+++ PeriodRender.py
@@ -2,54 +2,62 @@ import cv2
 import numpy as np
 
 
+class PeriodRender:
+
+    def __init__(self,I,D):
+
+        self.Image = cv2.imread('Input\\' + I,0)
+        self.height, self.width = self.Image.shape 
+        self.DownRes = D
+
+        self.WidthRatio = int(self.width/self.DownRes)
+        self.HeightRatio = int(self.height/self.DownRes)
+
+        self.PixelImage = [[] for _ in range(self.HeightRatio)]
 
 
 
-# Read the image
-image = cv2.imread('Input\Walter_White_S5B.png',0)
-height, width = image.shape 
-count = 0
-DownRes = 4
-PixelImage = [[] for _ in range(int(height/DownRes))]
+    def ConvertBinary(self):
+        for rows in range(self.height):
+            for pixel in range(self.width):
+                
+                if(self.Image[rows][pixel]>127):
 
-WaterWidth = int(width/DownRes)
-WaterHeight = int(height/DownRes)
+                    self.Image[rows][pixel] = 255
+                
+                elif(self.Image[rows][pixel]<=127):
 
-for rows in range(height):
-    for pixel in range(width):
-        
-        if(image[rows][pixel]>127):
+                    self.Image[rows][pixel] = 0
 
-            image[rows][pixel] = 255
-        
-        elif(image[rows][pixel]<=127):
-
-            image[rows][pixel] = 0
-
-
-
-for rows in range(WaterHeight):
-    
-    for pixel in range(WaterWidth):
-        
-        Current = image[rows*DownRes][pixel*DownRes] 
-
-        if(Current==255): #white
-            PixelImage[rows].append(" ")
-
-        elif(Current==0): #black
-            PixelImage[rows].append(".")
+        return self.Image
 
     
+    def FinalRender(self):
 
+        self.Image = self.ConvertBinary()
 
-with open('store.txt', 'w') as file:
-    for rows in range(WaterHeight):
-        for Pixel in range(WaterWidth):
-            file.write(PixelImage[rows][Pixel])
-        
-        file.write("\n")
+        for rows in range(self.HeightRatio):
+            
+            for pixel in range(self.WidthRatio):
+                
+                Current = self.Image[rows*self.DownRes][pixel*self.DownRes] 
+
+                if(Current==255): #white
+                    self.PixelImage[rows].append(" ")
+
+                elif(Current==0): #black
+                    self.PixelImage[rows].append(".")
+
+            
+        with open('store.txt', 'w') as file:
+            for rows in range(self.HeightRatio):
+                for Pixel in range(self.WidthRatio):
+                    file.write(self.PixelImage[rows][Pixel])
+                
+                file.write("\n")
 
 
 
 #cv2.imwrite('Output/output_image.jpg', PixelImage)
+
+PeriodRender(I="Walter_White_S5B.png",D=1).FinalRender()
